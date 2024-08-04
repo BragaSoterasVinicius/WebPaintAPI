5eadb3e02c11380992bdd025be382ed41312d3e6
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-08-04 14:30:24 -0300

    primeiro commit

---
+++ src/main/java/com/example/drawingapi/DrawingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.drawingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/drawingapi/DrawingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
